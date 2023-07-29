c3052a3e1c58a5bf8a10716cc623b04e2578618b
Pedro Emílio <103152070+Pebso@users.noreply.github.com>
2023-07-29 13:11:54 -0300

    Add files via upload

---
+++ vetor.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Estudos
+{
+    internal class Program
+    {
+        static void Main(string[] args)//vetor
+        {// split analisa o string original, e reconhece cada espaço em branco e separa o bloco de palavras
+            //armazenando em um vetor de string
+            string frase = Console.ReadLine();
+
+            string[] vet = frase.Split(' ');
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+            string p4 = vet[3];
+            string p5 = vet[4];
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+            Console.WriteLine(p5);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
