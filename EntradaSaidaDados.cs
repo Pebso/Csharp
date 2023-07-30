4e5b7686adc8542f16bcb4a75544c81e7e4abf7a
Pedro Emílio <103152070+Pebso@users.noreply.github.com>
2023-07-30 00:39:14 -0300

    Update EntradaSaidaDados.cs

--- EntradaSaidaDados.cs
+++ EntradaSaidaDados.cs
@@ -1,21 +1,24 @@
-﻿
 using System.Globalization;
 
 int n1 = int.Parse(Console.ReadLine());
 char ch = char.Parse(Console.ReadLine());
 double n2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
 
-string nome = Console.ReadLine();
-char sexo = char.Parse(Console.ReadLine());
-int idade = int.Parse(Console.ReadLine());
-double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+string[] vet = Console.ReadLine().Split( ' ' );// aspas simples para dividir nos espaços em branco
+string nome = vet[0];
+char sexo = char.Parse(vet[1]);// Parse para vetor tem o msm raciocinio do readline
+int idade = int.Parse(vet[2]);
+double altura = double.Parse(vet[3],CultureInfo.InvariantCulture);
 Console.WriteLine("voce digitou: ");
 Console.WriteLine(n1);
 Console.WriteLine(ch);
 Console.WriteLine(n2.ToString(), CultureInfo.InvariantCulture);
-
 Console.WriteLine(nome);
 Console.WriteLine(sexo);
 Console.WriteLine(idade);
-Console.WriteLine(altura.ToString(), CultureInfo.InvariantCulture);
+Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
 
